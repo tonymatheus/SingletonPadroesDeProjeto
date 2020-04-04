f7ca0b41667292f611f2b32be945dd0648be4627
tonymatheus <tony.chaves14@gmail.com>
2020-04-04 00:12:06 -0300

    Inicializando projeto

---
+++ src/com/upis/projetopadroes/Relogio.java
@@ -0,0 +1,21 @@
+package com.upis.projetopadroes;
+
+public class Relogio {
+	private static Relogio instanse;
+
+	private Data data;
+	private Horario horario;
+
+	private Relogio() {
+		data = new Data();
+		horario = new Horario();
+	}
+
+	public static Relogio getInstanse() {
+		if (instanse == null) {
+			instanse = new Relogio();
+		}
+		return instanse;
+	}
+
+}
